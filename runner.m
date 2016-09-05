7ddafe2f18631260aed65c86b0ebf96a4433e391
Erick <github@ericklavoie.com>
2016-09-05 16:56:05 +0200

    Modified the checksum computation

--- runner.m
+++ runner.m
@@ -35,16 +35,8 @@ output_delta = zeros(1,out);
 
 % load(net)
 nr = in;
-%input_units = createMatrixRandJS(1, nr); %C skips [0]
 input_units = zeros(1, nr);
 input_units(2:nr) = createMatrixRandJS(1, nr-1);
-%for i=2:nr
-%    input_units(i) = commonRandomJS(); %rand
-%end
-
-%disp(input_units);
-%disp(hidden_weights);
-%hidden_weights
 
 tic
 hidden_weights = bp_core(input_units,hidden_units,input_weights,...
@@ -52,35 +44,25 @@ hidden_weights = bp_core(input_units,hidden_units,input_weights,...
     hidden_prev_weights,input_prev_weights,hidden_weights);
 elapsedTime  = toc;
 
-%disp(hidden_weights);
-
-%hidden_weights
-
-if layer_size == expected_layer_size
-    for i=2:hidden_n+1
-        for j=2:output_n+1
-            sum_of_hidden_weights = sum_of_hidden_weights + hidden_weights(i,j);
-        end
+for i=2:hidden_n+1
+    for j=2:output_n+1
+        sum_of_hidden_weights = sum_of_hidden_weights + hidden_weights(i,j);
     end
-    valueA = floor(sum_of_hidden_weights*10e5);
-    valueB = floor(expected_sum_of_hidden_weights*10e5);
-    if valueA ~= valueB
-        error('ERROR: expected a sum of hidden weights of %f for an input size of %d but got %f instead\n',...
-            expected_sum_of_hidden_weights, expected_layer_size, sum_of_hidden_weights);
-    end
-else
-    warning('WARNING: no self-checking for input size of %d\n', layer_size);
 end
-%disp(', "output": ');
 
-EPS = 0.00001;
 
 disp('{');
 disp('"options":');
 disp(layer_size);
 disp(', "time": ');
 disp(elapsedTime);
+
+% Round the least significant bits of the answer by 
+% scaling up the sum proportionally to the layer_size 
+% and flooring the output, adjust by a given factor
+% to take the differences between implementations into account
+ADJUST = 0.1;
 disp(', "output":');
-disp(floor(sum_of_hidden_weights/EPS));
+disp(int32(floor((sum_of_hidden_weights*layer_size*ADJUST))));
 disp('}');
 end
